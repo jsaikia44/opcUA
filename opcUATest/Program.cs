49e8d910453300eba7ca4ff9f4dcd89fcd2afe40
jsaikia44 <jsaikia44@gmail.com>
2024-10-18 13:36:53 +0530

    file added

---
+++ opcUATest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+namespace opcUATest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var client = new OpcClient("opc.tcp://10.141.7.69:62640/IntegrationObjects/ServerSimulator");
+                client.Connect();
+
+               
+                while (true)
+                {
+                    OpcValue val = client.ReadNode("ns=2;s=Tag19");    
+                    Console.WriteLine("Value: {0}, Status: {1}", val, val.Status.ToString());
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
